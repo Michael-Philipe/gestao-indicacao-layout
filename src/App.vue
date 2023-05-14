344fbe3801c6a3f33bf4558f96a73a98eac8d0eb
Michael <michael.philipe2013@gmail.com>
2023-05-14 17:51:13 -0300

    table

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,32 @@
 <template>
-  
-  <SideBar/>
-  <Header/>
-
+  <SideBar />
+  <div class="container-fluid" style="padding: 0">
+    <Header />
+    <Table />
+  </div>
 </template>
 
 <script setup>
-import SideBar from './components/SideBar.vue'
-import Header from './components/Header.vue'
-
-
+import SideBar from './components/SideBar.vue';
+import Header from './components/Header.vue';
+import Table from './components/Table.vue';
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Poppins';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: #f3f3f3;
   margin-top: 60px;
   height: 100%;
   margin: 0;
   max-width: 1500px;
   display: flex;
 }
-a{
+a {
   padding: 0;
 }
 </style>
-
-
---
+++ src/components/Table.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container">
+    <table class="table table-bordeless">
+      <thead>
+        <tr class="thead">
+          <th>VEÍCULOS</th>
+          <th>!ÓLEOS</th>
+          <th>AÇÕES</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Volkswagen Fox Xtreme 1.6 Flex 8V 5p 2021</td>
+          <td>Texaco Havoline Ultra W Sae 5W40 SN</td>
+          <td>EDITAR</td>
+        </tr>
+        <tr>
+          <td>Volkswagen Fox Xtreme 1.6 Flex 8V 5p 2021</td>
+          <td>Texaco Havoline Ultra W Sae 5W40 SN</td>
+          <td>EDITAR</td>
+        </tr>
+        <tr>
+          <td>Volkswagen Fox Xtreme 1.6 Flex 8V 5p 2021</td>
+          <td>Texaco Havoline Ultra W Sae 5W40 SN</td>
+          <td>EDITAR</td>
+        </tr>
+        <tr>
+          <td>Volkswagen Fox Xtreme 1.6 Flex 8V 5p 2021</td>
+          <td>Texaco Havoline Ultra W Sae 5W40 SN</td>
+          <td>EDITAR</td>
+        </tr>
+        <tr>
+          <td>Volkswagen Fox Xtreme 1.6 Flex 8V 5p 2021</td>
+          <td>Texaco Havoline Ultra W Sae 5W40 SN</td>
+          <td>EDITAR</td>
+        </tr>
+        <tr>
+          <td>Volkswagen Fox Xtreme 1.6 Flex 8V 5p 2021</td>
+          <td>Texaco Havoline Ultra W Sae 5W40 SN</td>
+          <td>EDITAR</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TableComponent',
+};
+</script>
+
+<script setup></script>
+
+<style scoped>
+.thead {
+  background-color: #203972;
+  color: #ffffff;
+}
+.table {
+  border-radius: 12px;
+  overflow: hidden;
+  background-color: #ffffff;
+  text-align: left;
+}
+
+tr td {
+  border-radius: 6px;
+}
+</style>
