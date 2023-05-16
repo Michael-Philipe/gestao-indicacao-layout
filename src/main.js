059cadde2173e15811aec6a302034da071af0783
Michael <michael.philipe2013@gmail.com>
2023-05-16 11:14:17 -0300

    filtrar veiculo done

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
-createApp(App).use(bootstrap).mount('#app')
+
+const app = createApp(App)
+
+app.use(bootstrap)
+app.use(ElementPlus)
+app.mount('#app')
